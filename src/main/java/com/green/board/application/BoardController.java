fb17355d1e5891c874f210bcb724b3a02fedb334
woojeong <dayontheride@gmail.com>
2026-01-26 18:05:28 +0900

    260126 찐막

--- src/main/java/com/green/board/application/BoardController.java
+++ src/main/java/com/green/board/application/BoardController.java
@@ -14,8 +14,10 @@ public class BoardController {
     }
 
     @GetMapping("/board")
-    public String getBoard(@RequestParam int id) {
+    public String getBoard(@RequestParam int id
+                        , @RequestParam String title) {
         System.out.println("id:" + id);
+        System.out.println("title:" + title);
         return "아무거나";
     }
 }
